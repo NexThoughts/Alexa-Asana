22a688126e492c8d38fba8a9eeded5ec57da4600
Vijay <vshukla684@gmail.com>
2019-04-22 22:55:11 +0530

    add some dummy

--- grails-app/controllers/alexaasana/com/alexaasana/HomeController.groovy
+++ grails-app/controllers/alexaasana/com/alexaasana/HomeController.groovy
@@ -1,18 +1,26 @@
 package alexaasana.com.alexaasana
 
-import com.alexaasana.api.AsanaIntegration
 import com.alexaasana.co.TaskCO
+import com.alexaasana.request.AlexaRequest
+import grails.core.GrailsApplication
 
 class HomeController {
+    GrailsApplication grailsApplication
 
     def index() {
-        TaskCO taskCO = new TaskCO()
+        /*TaskCO taskCO = new TaskCO()
         taskCO.taskName = "My First Task"
         taskCO.projectName = "Demo Project"
         taskCO.tagName = "Test"
         taskCO.email = "anubhav@fintechlabs.in"
         AsanaIntegration asanaIntegration = new AsanaIntegration()
         asanaIntegration.createTask(taskCO)
-        render "DONE-------"
+        render "DONE-------"*/
+
+        TaskCO taskCO = new TaskCO(taskName: "A dummy task1", projectName: "Dummy Project1", tagName: "testdemo")
+
+        AlexaRequest alexaRequest = new AlexaRequest()
+        alexaRequest.processCreate(taskCO, grailsApplication.config.asana.access_token)
+        render "done"
     }
 }
